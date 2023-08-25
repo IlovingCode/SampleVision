918350c33d84c392330124ba48952c77467d4bfa
thien.nguyen5 <thien.nguyen5@mservice.com.vn>
2023-08-25 17:16:54 +0700

    - update

--- App.tsx
+++ App.tsx
@@ -6,17 +6,18 @@
  */
 
 import React from 'react';
-import { Button, Platform, TouchableHighlight } from 'react-native';
+import { Button, Image, Platform, View } from 'react-native';
 import { ArViewerView } from 'react-native-ar-viewer';
 import RNFS from 'react-native-fs';
 
 function App(): JSX.Element {
   const [localModelPath, setLocalModelPath] = React.useState<string>()
-  const extension = Platform.OS === 'android' ? 'glb' : 'usdz'
-  const modelSrc = `https://github.com/riderodd/react-native-ar/blob/main/example/src/dice.${extension}?raw=true`
+  const extension = Platform.OS === 'android' ? 'glb' : 'usdc'
+  const modelSrc = `https://static.momocdn.net/app/img/minigames/qr-models/SheenChair1.${extension}`
   const modelPath = `${RNFS.DocumentDirectoryPath}/model.${extension}`
   const ref = React.useRef() as React.MutableRefObject<ArViewerView>
-  const [detection, setDetection] = React.useState<number>(0)
+  const [detection, setDetection] = React.useState<number>(1)
+  const [detected, setDetected] = React.useState<string>(' ')
 
   const loadPath = async () => {
     const exists = await RNFS.exists(modelPath)
@@ -37,10 +38,22 @@ function App(): JSX.Element {
   const onChange = () => {
     let type = (detection + 1) % 2
 
-    type == 1 && ref.current.loadArImage('https://static.momocdn.net/app/img/Gamification/16032022.2x.jpg', 3)
     ref.current.setPlaneOrientation(type == 1 ? 'image' : 'horizontal')
+    type == 1 && ref.current.loadArImage([
+      'https://static.momocdn.net/app/img/Gamification/lacxi2023/unnamed.jpg',
+      'https://static.momocdn.net/app/img/Gamification/lacxi2023/IMG_0706.jpg',
+      'https://static.momocdn.net/app/img/Gamification/lacxi2023/IMG_0707.jpg',
+      'https://static.momocdn.net/app/img/Gamification/lacxi2023/IMG_0709.jpg',
+      // 'https://static.momocdn.net/app/img/Gamification/lacxi2023/IMG_0708.jpg',
+    ])
 
     setDetection(type)
+  }
+
+  const takeScreenshot = async () => {
+    console.log('takeScreenshot')
+    let res = await ref.current.takeScreenshot()
+    setDetected(res)
   }
 
   return <>
@@ -55,12 +68,24 @@ function App(): JSX.Element {
       // disableInstantPlacement
       onStarted={() => console.log('started')}
       onEnded={() => console.log('ended')}
-      onModelPlaced={() => console.log('model displayed')}
-      onModelRemoved={() => console.log('model not visible anymore')}
-      planeOrientation='horizontal'
+      onModelPlaced={(e) => setDetected(e.nativeEvent.detected)}
+      onModelRemoved={() => setDetected(' ')}
+      planeOrientation='image'
       ref={ref}
     />}
-    <Button onPress={onChange} title={detection == 0 ? 'Plane' : 'Image'} />
+    <Button onPress={takeScreenshot} title={detection == 0 ? 'Plane' : 'Image'} />
+    {detected.length > 1 && <View style={{
+      position: 'absolute', 
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#ffffff',
+      width: 200,
+      height: 300,
+      borderRadius: 10,
+      left: 100, top:100
+    }}>
+      <Image source={{uri: detected }} width={100} height={100} />
+    </View>}
   </>
 }
 
